332ec97a747ca5a2e1d54b3ef09a4a1f9e9dfdf5
oxzh <gabriel_alcantara12@outlook.com>
2024-02-27 15:35:48 -0300

    EXERCÍCIO

--- saidaDeDados/src/App.java
+++ saidaDeDados/src/App.java
@@ -45,7 +45,7 @@ public class App {
         // variável que seria declarada em seguida.
         // E assim como em váriaveis, precisamos declarar seu tipo, neste exemplo foi
         // utilizada o valor %f, que é de float, existe o %s de texto(string), e o %d de
-        // inteiro(int)
+        // inteiro(int), %n é uma quebra de linha e %c é para um char.
 
         // Aparentemente printf é apenas para strings, os outros tipos de print, não
         // permitem um %f ou algo do tipo dentro do texto, ao que eu percebi. Talvez
@@ -56,7 +56,30 @@ public class App {
         int idade = 18;
         double renda = 4000.0;
 
-        System.out.printf("O %s tem %d anos e ganha %.2f", nome, idade, renda);
+        System.out.printf("O %s tem %d anos e ganha %.2f%n%n", nome, idade, renda);
 
+        // EXERCICIO: fazer uma lista de produtos e em seguida aplicar um print com cada
+        // um dos produtos.
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'f';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $%.2f %n%n", product1, price1);
+        System.out.printf("%s, which price is %f %n%n ", product2, price2);
+
+        System.out.printf("Record: %d years old, code %d and gender %c %n%n", age, code, gender);
+
+        System.out.printf("Measure with eight decimal places: %.8f %n%n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f %n%n", measure);
+        System.out.printf("US decimal point: %.3f", measure);
     }
 }
