a848e9f75033c9fc4e24d7e53f3096d268e5230d
oxzh <gabriel_alcantara12@outlook.com>
2024-02-27 14:30:55 -0300

    data

--- saidaDeDados/src/App.java
+++ saidaDeDados/src/App.java
@@ -1,6 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        int idade = 18;
+        int caixaDeOvo = 30;
+        int caixaDeLeite = 30;
+        double pi = 3.14159265359;
+
         System.out.println("Hello World!");
         System.out.println("Bom Dia!");
+
+        System.out.print("A minha idade é: ");
+        System.out.println(idade);
+
+        System.out.print("A soma de uma caixa de ovo e uma caixa de leite é ");
+        System.out.print(caixaDeOvo + caixaDeLeite);
+
     }
 }
