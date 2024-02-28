b43a8b8334f898d1e6f04f798c519176f2bdc5c2
oxzh <gabriel_alcantara12@outlook.com>
2024-02-28 19:01:54 -0300

    javaCasting

---
+++ javaCasting/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int n1, n2;
+        n1 = 10;
+        n2 = 3;
+
+        double divisao = (double) n1 / n2;
+        System.out.println(divisao);
+
+        double a = 5.0;
+        int b = (int) a;
+
+        System.out.println(b);
+
+    }
+}
