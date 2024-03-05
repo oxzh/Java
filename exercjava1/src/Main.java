77981aa9f512dee81201829529de27a3a1091c9e
oxzh <gabriel_alcantara12@outlook.com>
2024-03-04 21:34:48 -0300

    exerc1java

---
+++ exercjava1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int A = 10;
+        int B = 30;
+        System.out.printf("Entrada dos valores são, %d e %d%n", A, B);
+
+
+        int soma = A + B;
+        System.out.printf("A soma dos valores, é = %d", soma);
+    }
+}
